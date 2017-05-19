3195dbd63b6c8b80d40080b671504e5b14cfbeeb
David Cheng <dvscheng@berkeley.edu>
2017-05-18 19:00:28 -0700

    first time

---
+++ Assets/Scripts/MapGenerator1.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator1 : MonoBehaviour {
+    int LENGTH = 21;
+    int HEIGHT = 3;
+    Sprite[] sprites;
+
+    private void Start()
+    {
+        sprites = Resources.LoadAll<Sprite>("Sprites/Scavengers_SpriteSheet");
+        makeObstacle();
+    }
+
+    private void makeRegularField()
+    {
+        for (int x = 0; x < LENGTH; x++)
+        {
+            for (int y = 0; y < HEIGHT; y++)
+            {
+                makeBlock(-LENGTH / 2 + x, -HEIGHT + y);
+            }
+        }
+    }
+
+    private void makeObstacle()
+    {
+        for (int x = 0; x < LENGTH; x++)
+        {
+            for (int y = 0; y < HEIGHT; y++)
+            {
+                makeBlock(-LENGTH / 2 + x, -HEIGHT + y);
+                if ((y + 1 == HEIGHT) && (x % Random.Range(1, 5) == 0) && (x != LENGTH/2))
+                {
+                    makeBlock(-LENGTH / 2 + x, -HEIGHT + y+1);
+                }
+            }
+        }
+    }
+
+    public void makeBlock(int x, int y)
+    {
+        GameObject ob = new GameObject();
+        SpriteRenderer renderer = ob.AddComponent<SpriteRenderer>();
+        Vector3 pos = new Vector3(x + 0.5f, y + 0.5f);
+
+        renderer.sprite = sprites[32];
+        renderer.name = "Dirt (" + x + ", " + y + ")";
+        //renderer.sprite = Resources.Load("Sprites/Scavengers_SpriteSheet", typeof(Sprite)) as Sprite;
+        ob.transform.position += pos;
+
+        BoxCollider2D boxCollider = ob.AddComponent<BoxCollider2D>();
+        boxCollider.sharedMaterial = Resources.Load("Materials/Frictionless", typeof(PhysicsMaterial2D)) as PhysicsMaterial2D;
+        ob.tag = "ground";
+    }
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    MapGenerator1 mg;
+    Rigidbody2D rb;
+    SpriteRenderer rendr;
+    bool facingRight;
+    bool canJump = true;
+    int NUM_COLLIDERS = 4;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rendr = GetComponent<SpriteRenderer>();
+        mg = FindObjectOfType(typeof(MapGenerator1)) as MapGenerator1;
+    }
+
+    private void Update()
+    {
+        /* Mouse checks. */
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector2 rayCoords = new Vector2(ray.origin.x, ray.origin.y);
+            Collider2D collider = Physics2D.OverlapCircle(rayCoords, 0.01f);
+            //RaycastHit hit;
+
+            //if (Physics.Raycast(ray, out hit, 100))
+            if (collider && collider.gameObject.name.CompareTo("Player") != 0)
+            {
+                //Object.Destroy(hit.transform.gameObject);
+                Object.Destroy(collider.gameObject);
+                //print("Collided");
+            }
+            else if (!collider)
+            {
+                mg.makeBlock((int) System.Math.Floor(rayCoords.x), (int) System.Math.Floor(rayCoords.y));
+                //print("Missed");
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        /* Controls (Physics). */
+        Vector2 currVelocity = rb.velocity;
+        bool movingLeft = Input.GetKey("left");
+        bool movingRight = Input.GetKey("right");
+        bool idle = !Input.GetKey("left") && !Input.GetKey("right");
+
+        if (movingLeft)
+        {
+            facingRight = false;
+            rb.velocity = new Vector2(-5, currVelocity.y);
+        } else if (movingRight)
+        {
+            facingRight = true;
+            rb.velocity = new Vector2(5, currVelocity.y);
+        } else if (idle)
+        {
+            rb.velocity = new Vector2(0, currVelocity.y);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            // maybe look in the vicinity of the character instead
+            var groundObjects = GameObject.FindGameObjectsWithTag("ground");
+            foreach (var ground in groundObjects)
+            {
+                var groundCollider = ground.GetComponent<BoxCollider2D>();
+                if (rb.IsTouching(groundCollider))
+                {
+                    rb.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+                    break;
+                }
+            }
+        }
+
+        rendr.flipX = facingRight;
+    }
+}
